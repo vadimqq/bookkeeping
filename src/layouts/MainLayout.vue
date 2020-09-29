0e0e3a049ea71d2ef3256fba33b026976a3e2e01
Vadim <66322242+vadimqq@users.noreply.github.com>
2020-09-29 21:45:54 +0400

    put into component sidebar and navbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,54 +1,8 @@
 <template>
   <div class="app-main-layout">
-    <nav class="navbar orange lighten-1">
-      <div class="nav-wrapper">
-        <div class="navbar-left">
-          <a href="#">
-            <i class="material-icons black-text">dehaze</i>
-          </a>
-          <span class="black-text">12.12.12</span>
-        </div>
-        <ul class="right hide-on-small-and-down">
-          <li>
-            <a class="dropdown-trigger black-text" href="#" data-target="dropdown">
-              USER NAME
-              <i class="material-icons right">arrow_drop_down</i>
-            </a>
-            <ul id='dropdown' class='dropdown-content'>
-              <li>
-                <a href="#" class="black-text">
-                  <i class="material-icons">account_circle</i>Профиль
-                </a>
-              </li>
-              <li class="divider" tabindex="-1"></li>
-              <li>
-                <a href="#" class="black-text">
-                  <i class="material-icons">assignment_return</i>Выйти
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <ul class="sidenav app-sidenav open">
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Счет</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">История</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Планирование</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Новая запись</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Категории</a>
-      </li>
-    </ul>
-    <main class="app-content">
+    <v-sidebar v-model="isOpen"/>
+    <v-navbar @click="isOpen= !isOpen"/>
+    <main class="app-content" :class="{full: !isOpen}">
       <div class="app-page">
         <router-view/>
       </div>
@@ -62,7 +16,17 @@
 </template>
 
 <script>
+import Sidebar from '@/components/ui-kit/Sidebar.vue'
+import Navbar from '@/components/ui-kit/Navbar.vue'
+
 export default {
-  name: 'MainLayout'
+  name: 'MainLayout',
+  data: () => ({
+    isOpen: true
+  }),
+  components: {
+    'v-sidebar': Sidebar,
+    'v-navbar': Navbar
+  }
 }
 </script>
