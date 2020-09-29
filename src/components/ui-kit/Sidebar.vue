f41768b18e4e3a3f00bc34df9b053e4486a17430
Vadim <66322242+vadimqq@users.noreply.github.com>
2020-09-29 21:44:16 +0400

    complite sidebar and navbar

---
+++ src/components/ui-kit/Sidebar.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: value}">
+    <router-link tag="li" active-class="active" :to="link.href" :exact="link.exact" v-for="link in links" :key="link.url">
+      <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: [
+    'value'
+  ],
+  data: () => ({
+    links: [
+      {
+        title: 'счет',
+        href: '/',
+        exact: true
+      },
+      {
+        title: 'История',
+        href: '/history'
+      },
+      {
+        title: 'Планирование',
+        href: '/planning'
+      },
+      {
+        title: 'Новая запись',
+        href: '/record'
+      },
+      {
+        title: 'Категории',
+        href: '/categories'
+      }
+    ]
+  })
+}
+</script>
