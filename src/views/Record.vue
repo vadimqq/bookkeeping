fce33c07a1f9af5c5b7f2df3c1117b5d60261831
Vadim <66322242+vadimqq@users.noreply.github.com>
2020-10-02 11:07:37 +0400

    complite Record page

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -5,7 +5,7 @@
     </div>
     <Preloader v-if="loading"/>
     <p class="center" v-else-if="!categories.length">Категорий пока что нет.<router-link to="/сategories">Добавьте новую категорию</router-link></p>
-    <form class="form" v-else>
+    <form class="form" v-else @submit.prevent="onSubmit">
       <div class="input-field" >
         <select ref="select" v-model="category">
           <option v-for="item in categories" :key="item.id" :value="item.id">{{item.title}}</option>
@@ -25,14 +25,14 @@
         </label>
       </p>
       <div class="input-field">
-        <input id="amount" type="number" v-model.number="amount"/>
+        <input id="amount" type="number" v-model.number="amount" :class="{invalid: $v.amount.$dirty && !$v.amount.minValue}"/>
         <label for="amount">Сумма</label>
-        <span class="helper-text invalid">amount пароль</span>
+        <span class="helper-text invalid" v-if="$v.amount.$dirty && !$v.amount.minValue">Введите минимальную сумму</span>
       </div>
       <div class="input-field">
-        <input id="description" type="text" v-model="description">
+        <input id="description" type="text" v-model="description" :class="{invalid: $v.description.$dirty && !$v.description.required}">
         <label for="description">Описание</label>
-        <span class="helper-text invalid">description пароль</span>
+        <span class="helper-text invalid" v-if="$v.description.$dirty && !$v.description.required">Введите описание</span>
       </div>
       <button class="btn waves-effect waves-light" type="submit"> Создать <i class="material-icons right">send</i></button>
     </form>
@@ -40,7 +40,8 @@
 </template>
 
 <script>
-
+import { required, minValue } from 'vuelidate/lib/validators'
+import { mapGetters } from 'vuex'
 export default {
   name: 'Record',
   data: () => ({
@@ -60,7 +61,49 @@ export default {
     }
     setTimeout(() => {
       this.select = window.M.FormSelect.init(this.$refs.select)
+      window.M.updateTextFields()
     }, 0)
+  },
+  validations: {
+    amount: { minValue: minValue(100) },
+    description: { required }
+  },
+  computed: {
+    ...mapGetters(['info']),
+    canCreateRecord () {
+      if (this.type === 'income') {
+        return true
+      }
+      return this.info.bill >= this.amount
+    }
+  },
+  methods: {
+    async onSubmit () {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+      }
+      if (this.canCreateRecord) {
+        try {
+          await this.$store.dispatch('createRecord', {
+            categoryId: this.category,
+            amount: this.amount,
+            description: this.description,
+            type: this.type,
+            date: new Date().toJSON()
+          })
+          const bill = this.type === 'income'
+            ? this.info.bill + this.amount
+            : this.info.bill - this.amount
+          await this.$store.dispatch('updateInfo', { bill })
+          this.$message('Запись успешно добавлена')
+          this.$v.$reset()
+          this.amount = 1
+          this.description = ''
+        } catch (e) {}
+      } else {
+        this.$message(`Не достаточтно средств на счете (${this.amount - this.info.bill})`)
+      }
+    }
   },
   destroyed () {
     if (this.select && this.select.destroy) {
